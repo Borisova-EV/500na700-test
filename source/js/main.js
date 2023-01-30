03aeb4382c43040aa6a53a17efb86de2a06d6c6c
Евгения Борисова <borisova.ev.2019@yandex.ru>
2023-01-30 19:00:37 +0300

    Добавляет форму на страницу

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,10 @@
 import './vendor/swiper';
+import IMask from './vendor/imask';
 import { initSlider } from './modules/init-slider';
 import { initAccordeon } from './modules/init-accordeon';
+import { initMaskTelephone } from './modules/init-mask-telephone';
 
 initSlider();
 initAccordeon();
+initMaskTelephone();
 
